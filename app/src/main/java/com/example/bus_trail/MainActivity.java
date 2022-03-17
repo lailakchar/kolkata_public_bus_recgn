5108c54cc53172fb95e3200b6c2050988f0262eb
UniversityManagementSystem <universitysystem21@gmail.com>
2022-03-17 22:31:24 +0530

    activity_main.xml Page Done!

--- app/src/main/java/com/example/bus_trail/MainActivity.java
+++ app/src/main/java/com/example/bus_trail/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.bus_trail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
     }
 }
